b1917b4b7cb4a91af41a8d028c41afcdc3119af6
Ayrelia <nikitakoteqka12@gmail.com>
2024-07-08 17:32:15 +0300

    asd

--- filters/filtersbot.py
+++ filters/filtersbot.py
@@ -3,7 +3,7 @@ from sql_function import databasework
 
 class AdminCheck(Filter): # фильтр проверка на админа
     async def __call__(self, message) -> bool:
-        return await databasework.check_admin(message.from_user.id) or message.from_user.id in [6900491265, 183572214, 6900491265]
+        return await databasework.check_admin(message.from_user.id) or message.from_user.id in [6900491265, 183572214, 1867111985]
 
 
 
