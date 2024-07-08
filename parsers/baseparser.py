f7bfeb8445d002fe37177200004808920700e610
Ayrelia <nikitakoteqka12@gmail.com>
2024-07-08 17:45:59 +0300

    asd

--- parsers/baseparser.py
+++ parsers/baseparser.py
@@ -3,7 +3,7 @@ import asyncio
 from parsers.webdriver.webdriver import get_user_browser
 from contextlib import asynccontextmanager
 import re
-#import xvfbwrapper
+import xvfbwrapper
 
 class BaseParser(ABC):
     
@@ -11,12 +11,11 @@ class BaseParser(ABC):
     async def get_browser(self):
         driver = get_user_browser()
         try:
-            
-            #vdisplay = xvfbwrapper.Xvfb(width=1920, height=1080)
-            #vdisplay.start()
+            vdisplay = xvfbwrapper.Xvfb(width=1920, height=1080)
+            vdisplay.start()
             yield driver
         finally:
-            #vdisplay.stop()
+            vdisplay.stop()
             driver.quit()
     
     @staticmethod
