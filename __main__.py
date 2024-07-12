4e4e4f975d5151e81e4e55c2854b1f31ac0b45ee
Ayrelia <nikitakoteqka12@gmail.com>
2024-07-12 11:53:21 +0300

    upd

--- __main__.py
+++ __main__.py
@@ -27,7 +27,7 @@ async def start_commands(bot: Bot):
     await bot.set_my_commands(commands, BotCommandScopeDefault())
 
     scheduler = AsyncIOScheduler(timezone='Europe/Moscow')
-    scheduler.add_job(check_price_token, 'interval', seconds=120)
+    scheduler.add_job(check_price_token, 'interval', seconds=10)
     scheduler.start()
     
     task1 = asyncio.create_task(create_tables()) # создаем базу юзеров если нет
