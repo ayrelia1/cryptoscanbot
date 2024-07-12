9c60edf6c0ce4d7db520ef3427605d0799d9913c
Ayrelia <nikitakoteqka12@gmail.com>
2024-07-12 21:41:46 +0300

    upd

--- parsers/check_price.py
+++ parsers/check_price.py
@@ -26,7 +26,7 @@ async def check_price_token():
                 
                 if result_x['percent_change'] > 99 and result_x['percent_change'] < 3001:
                     
-                    photo_path = current_directory + f"/assets/{str(int(result_x['percent_change']))}.jpg"
+                    photo_path = current_directory + f"/assets/{str(result_x['percent_change'])}.jpg"
                     
                     async with Client("my_account", api_id=settings.API_ID, api_hash=settings.API_HASH) as app:
                         await app.send_photo(caption=text, photo=photo_path, chat_id=channel_id, reply_to_message_id=message_id)
@@ -60,16 +60,18 @@ async def get_token_price(address):
     
 
 async def track_token_price(id_token, initial_price, address, max_notified_multiplier):
-
-
+       
+      
     try:
         current_price, mcap, symbol = await get_token_price(address)
         current_price = float(format_number(float(current_price)))
         initial_price = float(format_number(float(initial_price)))
         multiplier = current_price / initial_price
-        percent_change = (multiplier - 1) * 100
-
-        # Пороговые значения процентов для проверки
+        # Проверка новых кратностей
+        new_multiplier = int(multiplier)
+        percent_change = (new_multiplier - 1) * 100
+        
+                # Пороговые значения процентов для проверки
         thresholds = [(1.4, 1,59), (1.6, 1.79), (1.8, 1.99)]
         
         # Проверка процентов изменения
@@ -78,20 +80,19 @@ async def track_token_price(id_token, initial_price, address, max_notified_multi
                 
                 # Обновление максимальной достигнутой кратности, если превышает текущую
                 if round(multiplier, 1) > max_notified_multiplier:
-                    await databasework.update_max_notified_multiplier_token(round(multiplier, 1), id_token)
-                    max_notified_multiplier = multiplier
+                    await databasework.update_max_notified_multiplier_token(threshold[1], id_token)
+                    
 
                     data = {
-                        "new_multiplier": multiplier,
+                        "new_multiplier": round(multiplier, 1),
                         "mcap": mcap,
                         "current_price": current_price,
                         "symbol": symbol,
                         "percent_change": percent_change
                     }
                     return data
-
-        # Проверка новых кратностей
-        new_multiplier = int(multiplier)
+        
+        
         if new_multiplier > 100:
             return None
         if new_multiplier > max_notified_multiplier:
@@ -105,8 +106,12 @@ async def track_token_price(id_token, initial_price, address, max_notified_multi
                 "symbol": symbol,
                 "percent_change": percent_change
             }
+            
             return data
-
+        
+        
+        
         return None
+      
     except Exception as e:
         logging.error(f"An error occurred: {e}")
