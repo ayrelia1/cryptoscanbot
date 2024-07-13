2702baa60c851ab29acf84a518cf85300f4426d6
Ayrelia <nikitakoteqka12@gmail.com>
2024-07-13 12:36:07 +0300

    upd

--- parsers/check_price.py
+++ parsers/check_price.py
@@ -72,10 +72,10 @@ async def track_token_price(id_token, initial_price, address, max_notified_multi
         new_multiplier_float = round(multiplier, 1)
         percent_change = (new_multiplier - 1) * 100
         
-        crat = [(1.4, 1.5), (1.6, 1.7), (1.8, 1.9)]
+        crat = [(1.4, 1.59999), (1.6, 1.79999), (1.8, 1.9999)]
         
         for i in crat:
-            if new_multiplier_float >= i[0] and new_multiplier_float <= i[1] and new_multiplier_float > max_notified_multiplier:
+            if (new_multiplier_float >= i[0] and new_multiplier_float <= i[1]) and new_multiplier_float > max_notified_multiplier:
                 await databasework.update_max_notified_multiplier_token(i[1], id_token)
             
                 data = {
