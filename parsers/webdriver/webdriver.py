2d6d7531d504992635f8130c2379058ccf7c5a92
ayrelia1 <nikitakoteqka12@gmail.com>
2024-09-10 18:45:17 +0300

    upd

--- parsers/webdriver/webdriver.py
+++ parsers/webdriver/webdriver.py
@@ -3,7 +3,7 @@ from selenium.webdriver import Chrome, ChromeOptions
 from config import current_directory
 
 def get_user_browser():
-    chrome_driver_path = Service('parsers/webdriver/chromedriver')
+    chrome_driver_path = Service(current_directory + 'parsers/webdriver/chromedriver')
     options = ChromeOptions()
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option('useAutomationExtension', False)
